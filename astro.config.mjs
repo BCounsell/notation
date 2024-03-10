0c815e3127f049454411cad0d581a65c73b6f856
Ben Counsell <ben@basebandlabs.com>
2024-03-09 16:10:58 -0800

    Synced Notation

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bcounsell.github.io/notation",
-  // base: "/notation",
+  base: "/notation",
   integrations: [mdx(), sitemap(), tailwind()],
 });
